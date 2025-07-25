38a331e2168307abcabd6bd5b7241a48c8167a68
MohamedShihaa <120946004+MohamedShihaa@users.noreply.github.com>
2025-07-25 16:40:38 +0200

    Create build.gradle.kts

---
+++ faselhd/src/main/kotlin/com/mohamedshihaa/faselhd/build.gradle.kts
@@ -0,0 +1,7 @@
+cloudstream {
+    language = "kotlin"
+    pluginId = "faselhd.plugin"
+    name = "FaselHD"
+    description = "Adds support for FaselHD (faselhd.link)"
+    authors = listOf("MohamedShihaa")
+}
